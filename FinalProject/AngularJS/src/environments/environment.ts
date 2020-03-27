2226683ba6f3eeb2d1155be59c1194d3fdc9b89a
unknown <erlynetv@gmail.com>
2020-03-26 23:27:34 -0400

    Se añadió el proyecto de angular para hacer el FrontEnd

---
+++ FinalProject/AngularJS/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDHLQjfFtDUxdte5WllCA4Woj4Iy69MPKc",
+    authDomain: "restaurants-38b77.firebaseapp.com",
+    databaseURL: "https://restaurants-38b77.firebaseio.com",
+    projectId: "restaurants-38b77",
+    storageBucket: "restaurants-38b77.appspot.com",
+    messagingSenderId: "899856212347",
+    appId: "1:899856212347:web:4041d2bf7cbf4586a61dde"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
