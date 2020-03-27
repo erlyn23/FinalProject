287c527107dd0be8a92f9a683af3491b31ca422d
unknown <erlynetv@gmail.com>
2020-03-26 21:45:02 -0400

    Se creó el controlador CitasController

---
+++ FinalProject/Controllers/CitasController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class CitasController : ApiController
+    {
+        private SistemaMedicoEntities db = new SistemaMedicoEntities();
+        
+        [ResponseType(typeof(void))]
+        [Route("AgregarCita")]
+        public IHttpActionResult AddCita(Citas cita, Medicos medico, Pacientes paciente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            cita.idPaciente = paciente.idPaciente;
+            cita.idMedico = medico.idMedico;
+
+            db.Citas.Add(cita);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cita.idCita }, cita);
+        }
+    }
+}
--- FinalProject/Utilidades/UtilidadesMedicos.cs
+++ FinalProject/Utilidades/UtilidadesMedicos.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FinalProject.Models;
 
 namespace FinalProject.Utilidades
 {
     public class UtilidadesMedicos
     {
-        public static void IngresarPaciente() 
+        public static void IngresarPaciente(Pacientes paciente) 
         { 
             
         }
