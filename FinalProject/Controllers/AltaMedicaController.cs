293f8ea8ad8126fce87a6f9451ded5866f1c7445
unknown <erlynetv@gmail.com>
2020-03-31 19:13:44 -0400

    Se creó el controlador AltasMedicas y la clase para su extracción de datos

---
+++ FinalProject/Controllers/AltaMedicaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Data.SqlClient;
+using FinalProject.Models;
+
+
+namespace FinalProject.Controllers
+{
+    public class AltaMedicaController : ApiController
+    {
+        SistemaMedicoEntities db = new SistemaMedicoEntities();
+        SqlConnection conexion = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+    }
+}
---
+++ FinalProject/Models/AltaArreglada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class AltaArreglada
+    {
+        public int idAltaMedica { get; set; }
+        public string NombrePaciente { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaSalida { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
