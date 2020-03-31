158fab2e5741f01f3b8cddf8af85caced62e709f
unknown <erlynetv@gmail.com>
2020-03-31 00:35:12 -0400

    Se crearon los métodos del CRUD de habitaciones

--- FinalProject/AngularJs/src/app/services/habitaciones.service.ts
+++ FinalProject/AngularJs/src/app/services/habitaciones.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Habitaciones } from '../Models/Habitaciones';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HabitacionesService {
 
-  constructor() { }
+  url = "https://localhost:44347/api/Habitaciones"
+  constructor(private http: HttpClient) { }
+
+  ObtenerHabitaciones(): Observable<Habitaciones[]>{
+    return this.http.get<Habitaciones[]>(this.url);
+  }
+
+  AgregarHabitacion(habitacion: Habitaciones): Observable<Habitaciones>{
+    const httpOptions = {headers: new HttpHeaders({"Content-type":"application/json"})};
+    return this.http.post<Habitaciones>(this.url, habitacion, httpOptions)
+  }
+
+  ModificarHabitacion(habitacion:Habitaciones): Observable<Habitaciones>{
+    const httpOptions = {headers: new HttpHeaders({"Content-type":"application/json"})};
+    return this.http.put<Habitaciones>(this.url, habitacion, httpOptions);
+  }
+
+  EliminarHabitacion(id: string): Observable<number>{
+    const httpOptions = {headers: new HttpHeaders({"Content-type":"application/json"})};
+    return this.http.delete<number>(this.url+"/?id="+id,httpOptions);
+  }
 }
